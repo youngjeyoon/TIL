d000f09d66c167035481d5aa255259e474ccd223
youngjeyoon <yyj5677@naver.com>
2022-05-27 16:17:31 +0900

    2022-05-27

---
+++ 03/2022-05-27/2022-05-27/소스.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	// 쉽게 배우는 C언어 Chapter 2 programing 연습문제
+	// 01
+	
+	printf("이름: 홍길동\n");
+	printf("나이: 21살\n");
+	printf("주소: 서울 200번지");
+
+	return 0;
+}
---
+++ 03/2022-05-27/Project1/소스.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	// 02
+	printf("Hello\nC\nProgrammers!");
+	
+	return 0;
+}
